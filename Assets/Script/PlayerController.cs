2f669525823a9ee328ce3e58021e84af406d43ca
CrypticSocket <saket.vaibhav1197@gmail.com>
2020-10-02 18:15:36 +0530

    Chaos being Chaotic

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float movementSpeed = 10f;
+    [SerializeField]
+    private float mouseSensitivityHorizontal = 5f;
+    [SerializeField]
+    private float mouseSensitivityVertical = 5f;
+    [SerializeField]
+    private float jumpForce = 50f;
+
+
+    private PlayerMotor motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _xmov = Input.GetAxisRaw("Horizontal");   //Left right
+        float _zmov = Input.GetAxisRaw("Vertical");     //Front back
+
+        Vector3 _movHorizontal = transform.right * _xmov;
+        Vector3 _movVertical = transform.forward * _zmov;
+
+        Vector3 velocity = (_movHorizontal + _movVertical).normalized * movementSpeed;
+        motor.Move(velocity);
+
+        float _yrot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0, _yrot, 0) * mouseSensitivityHorizontal;
+
+        motor.LookHorizontal(_rotation);
+
+        float _xrot = Input.GetAxisRaw("Mouse Y");
+        Vector3 _cameraRotation = new Vector3(_xrot, 0, 0) * mouseSensitivityVertical;
+        motor.LookVertical(_cameraRotation);
+
+        float _jumpY = Input.GetAxisRaw("Jump");
+        Vector3 _jumpForce = new Vector3(0, _jumpY, 0) * jumpForce;
+        motor.Jump(_jumpForce);
+
+    }
+}
---
+++ Assets/Script/PlayerMotor.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+
+    [SerializeField]
+    private Camera cam;
+
+    private Rigidbody rb;
+    private Vector3 movementSpeed;
+    private Vector3 horizontalRotationSpeed;
+    private Vector3 verticalRotationSpeed;
+    private Vector3 jumpForce;
+    private bool isGrounded;
+
+    [SerializeField]
+    private float fallSpeedMultiplier = 2.5f;
+    public Transform groundCheck;
+    public float gravity = -9.81f;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+    }
+
+    public void Move(Vector3 speed)
+    {
+        movementSpeed = speed;
+    }
+
+    public void LookHorizontal(Vector3 speed)
+    {
+        horizontalRotationSpeed = speed;
+    }
+
+    public void LookVertical(Vector3 speed)
+    {
+        verticalRotationSpeed = speed;
+    }
+
+    public void Jump(Vector3 force)
+    {
+        jumpForce = force;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformLookHorizontal();
+        PerformLookVertical();
+        PerformJump();
+    }
+
+    void PerformMovement()
+    {
+        if (movementSpeed != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + movementSpeed * Time.deltaTime);
+            anim.SetInteger("wri", 1);
+        }
+        else
+        {
+            anim.SetInteger("wri", 0);
+        }
+    }
+
+    void PerformLookHorizontal()
+    {
+        rb.MoveRotation(transform.rotation * Quaternion.Euler(horizontalRotationSpeed));
+    }
+
+    void PerformLookVertical()
+    {
+        if(cam!=null)
+        {
+            cam.transform.Rotate(-verticalRotationSpeed);
+        }
+    }
+
+    void PerformJump()
+    {
+        Debug.Log(isGrounded);
+        if (isGrounded && jumpForce != Vector3.zero)  //Add if touching to ground
+        {
+            rb.AddForce(jumpForce * Time.deltaTime, ForceMode.Impulse);
+            anim.SetInteger("wri", 2);
+
+            //Debug.Log("Jumping");
+
+           /* if(rb.velocity.y < 0)
+            {
+                rb.velocity += Vector3.up * gravity * fallSpeedMultiplier * Time.deltaTime; 
+            }*/
+        }
+    }
+}
